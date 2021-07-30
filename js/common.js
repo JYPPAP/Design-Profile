bc4e43f70911540169e28e544f76e42ede84cc9f
JoYoungSu <yungsu222@naver.com>
2021-07-30 17:26:21 +0900

    HTML 1. 인트로모달 내용 수정 2. 프론트모달, 백모달, 개발모달 생성 및 수정 3. 포트폴리오1 텍스트 수정 4. 앱카드 내용 변경 5. Contact Me를 개발자와 대화하기로 수정 및 자동응답 멘트 변경  SCSS 1. 변수 --circlr-red, -yellow, -green 추가 2.  라이트모드용 SCSS 적용 3. body에 light 클래스가 들어갔을 때 토글버튼에 트랜지션과 변경 추가. 4. blackOn 클래스를 삭제하고, display:none을 제거, visibility 로 변경, z-index:0으로 수정 5. 필요없는 주석 제거. 6. 인트로모달에 word-wrap 과 word-break 추가. 7. 모달의 포지션을 fixed로 변경 8. 종료했을 때 프레임만 남는걸 수정하기 위해 transition 전부 삭제 9. 인트로모달에 overflow-y: auto를 줘서 모바일화면에서 화면스크롤이 가능하게 변경. 10.인트로모달  모바일화면에서 화면 높이에 따른 미디어쿼리 생성 JS 1. 다크모드(라이트모드)용 스크립트 추가 2. 기존의 jQuery를 수정 - 헤더메뉴 클릭했을 때 is-active의 적용에 대해서 수정 - main-header-link를 클릭했을 때 is-active 클래스 적용을 배열로 클래스를 받아다가 배열별로 적용하는 형태로 제작함. 3. 다중모달에 대해서 스크립트 생성 완료

--- js/common.js
+++ js/common.js
@@ -1,25 +1,37 @@
-$(function () {
-  $(".header-menu-link").click(function () {
-    $(".header-menu-link").removeClass("is-active");
-    $(this).addClass("is-active");
-  });
+/* jshint esversion: 6 */
+
+/* headerMenu 클릭했을 때 */
+document.addEventListener("DOMContentLoaded", () => {
+  const headerMenu = document.getElementsByClassName('header-menu-link');
+
+  headerMenu[0].onclick = function () {
+    headerMenu[1].classList.remove('is-active');
+    this.classList.add('is-active');
+  }
+  headerMenu[1].onclick = function () {
+    headerMenu[0].classList.remove('is-active');
+    this.classList.add('is-active');
+  }
 });
 
-$(function () {
-  $(".main-header-link").click(function () {
-    $(".main-header-link").removeClass("is-active");
-    $(this).addClass("is-active");
-  });
-});
 
-const dropdowns = document.querySelectorAll(".dropdown");
-dropdowns.forEach((dropdown) => {
-  dropdown.addEventListener("click", (e) => {
-    e.stopPropagation();
-    dropdowns.forEach((c) => c.classList.remove("is-active"));
-    dropdown.classList.add("is-active");
-  });
-});
+/* search-bar input 클릭했을 때 */
+// document.addEventListener("DOMContentLoaded", () => {
+//   const searchBar = document.querySelector(".search-bar input");
+//   const headerMenu =  document.querySelector(".header");
+
+//   console.log(searchBar);
+//   console.log(headerMenu);
+
+//   searchBar.focus = function () {
+//     console.log("focus is on");
+//     headerMenu.classList.add('wide');
+//   };
+//   searchBar.blur = function () {
+//     console.log("blur is on");
+//     headerMenu.classList.remove('wide');
+//   };
+// });
 
 $(".search-bar input")
   .focus(function () {
@@ -29,63 +41,99 @@ $(".search-bar input")
     $(".header").removeClass("wide");
   });
 
-$(document).click(function (e) {
-  var container = $(".status-button");
-  var dd = $(".dropdown");
-  if (!container.is(e.target) && container.has(e.target).length === 0) {
-    dd.removeClass("is-active");
+/* 화면 너비 변경용 */
+document.addEventListener("DOMContentLoaded", () => {
+  const changeWidth = document.getElementsByClassName('main-header-link');
+  console.log(changeWidth);
+
+  changeWidth[0].onclick = function () {
+    changeWidth[1].classList.remove('is-active');
+    changeWidth[2].classList.remove('is-active');
+    changeWidth[0].classList.add('is-active');
+    console.log('DesktopSize is working');
+  }
+  changeWidth[1].onclick = function () {
+    changeWidth[0].classList.remove('is-active');
+    changeWidth[2].classList.remove('is-active');
+    changeWidth[1].classList.add('is-active');
+    console.log('TabletSize is working');
+  }
+  changeWidth[2].onclick = function () {
+    changeWidth[0].classList.remove('is-active');
+    changeWidth[1].classList.remove('is-active');
+    changeWidth[2].classList.add('is-active');
+    console.log('MobileSize is working');
+  }
+});
+  /* 모달페이지 열기. */
+document.addEventListener("DOMContentLoaded", () => {
+  const bodyBlackWrap = document.querySelector('.body-blackWrap');
+  const popupModal = document.getElementsByClassName('popup-modal');
+  const modalBtn = document.getElementsByClassName('modal-button');
+  const closeBtn = document.getElementsByClassName('popup-modal__close-button');
+  let funcs = [];
+
+  function Modal(num) {
+    return function () {
+      modalBtn[num].onclick = function() {
+        popupModal[num].style.visibility = "visible";
+        bodyBlackWrap.style.visibility = "visible"
+      };
+
+      closeBtn[num].onclick = function() {
+        bodyBlackWrap.style.visibility = "hidden";
+        popupModal[num].style.visibility = "hidden";
+      };
+    };
+  }
+
+  for(let i =0; i < popupModal.length; i++) {
+    funcs[i] = Modal(i);
+  }
+  for(let j = 0; j < popupModal.length; j++) {
+    funcs[j]();
   }
 });
 
-$(function () {
-  $(".dropdown").on("click", function (e) {
-    $(".content-wrapper").addClass("overlay");
-    e.stopPropagation();
-  });
-  $(document).on("click", function (e) {
-    if ($(e.target).is(".dropdown") === false) {
-      $(".content-wrapper").removeClass("overlay");
-    }
-  });
+/* light mode */
+document.addEventListener('DOMContentLoaded', () => {
+
+  /* 로컬 스토리지에서 theme 받아오기 */
+  let theme = localStorage.getItem('theme');
+  /* 만약 theme의 값이 없다면 */
+  if(!theme) {
+    /* theme을 light로 지정 */
+    // localStorage.setItem('theme', 'light');
+    /* matches에 prefers-color-scheme값을 dark를 matches에 입력 */
+    const { matches } = window.matchMedia('(prefers-color-scheme: dark)');
+    // console.log(matches);
+    /* matches의 값을 dark와 light중에 비교해서 theme에 입력. */
+    theme = matches ? 'dark' : 'light';
+    /* 로컬스토리지에 theme을 위에서 입력한 theme로 적용. */
+    localStorage.setItem('theme', theme);
+  }
+  /* theme의 값이 light 면 light 클래스를 추가하고, 아니면 추가하지 않는 코드. */
+  document.body.classList.toggle('light', theme === 'light');
+  
+  /* 깜빡거림 제거 (다크모드의 토글버튼에 걸려있는 트랜지션이 .3s이기 때문에 여기에서 setTimeout에 300ms의 딜레이를 적용했다.) */
+  setTimeout(() => {
+    document.body.style.visibility = 'visible';
+  }, 300);
 });
+/* 토글버튼에 클릭이벤트가 발생하면 */
+document.querySelector('.toggle-button').onclick = e => {
+  /* 로컬스토리지에서 theme값을 가져와서 theme에 저장 */
+  const theme = localStorage.getItem('theme');
+  /* 로컬스토리지에 theme키값에 value는 theme값을 light와 dark를 비교해서 light면 theme에 light를 적용 */
+  localStorage.setItem('theme', `${theme === 'light' ? 'dark' : 'light'}`);
+  /* body 태그에 light클래스를 토글적용. = 버튼 1개로 처리하기 때문에. */
+  document.body.classList.toggle('light');
+};
+/* js에서 다크모드 감지. */
+// const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+// console.log(darkModeMediaQuery);
 
-$(function () {
-  $(".status-button:not(.open)").on("click", function (e) {
-    $(".overlay-app").addClass("is-active");
-  });
-  $(".pop-up .close").click(function () {
-    $(".overlay-app").removeClass("is-active");
-  });
-});
-
-$(".status-button:not(.open)").click(function () {
-  $(".pop-up").addClass("visible");
-});
-
-$(".pop-up .close").click(function () {
-  $(".pop-up").removeClass("visible");
-});
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  const bodyBlackWrap = document.querySelector('.body-blackWrap')
-  const popupModal = document.querySelector('.popup-modal')
-  const introModal = document.querySelector('.content-wrapper-context .모달 이름넣기.')
-
-
-  /* 인트로 팝업창. */
-  document.querySelector('.content-wrapper-context .content-button').addEventListener('click', function() {
-    introModal.classList.add('is--visible');
-    bodyBlackWrap.classList.add('blackOn');
-  });
-
-  /* 모달 닫기 */
-  popupModal.querySelector('.popup-modal__close').addEventListener('click', () => {
-    popupModal.classList.remove('is--visible');
-    bodyBlackWrap.classList.remove('blackOn');
-  });
-  bodyBlackWrap.addEventListener('click', () => {
-    popupModal.classList.remove('is--visible');
-    bodyBlackWrap.classList.remove('blackOn');
-  });
-});
+// darkModeMediaQuery.addEventListener(e => {
+//   const darkModeOn = e.matches;
+//   console.log(`Dark mode is ${darkModeOn ? 'on' : 'off'}.`);
+// });
